3e21b0d9fb9e7e395dc4085a20426e41da6e76e0
koefficiensdev <sandorturai178@gmail.com>
2026-03-29 19:38:57 +0200

    firebase added
    I added the firebase to the js

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCnqITLPpjDiEtuA_FH90YtATgZVsX6oOg",
+  authDomain: "ovexi-2f197.firebaseapp.com",
+  projectId: "ovexi-2f197",
+  storageBucket: "ovexi-2f197.firebasestorage.app",
+  messagingSenderId: "545150304866",
+  appId: "1:545150304866:web:7156a64b44ddc02126073b",
+  measurementId: "G-W7W68C38VQ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+
 // Navbar scroll effect
 window.addEventListener('scroll', () => {
     const navbar = document.getElementById('navbar');
