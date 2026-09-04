e8e7bf72d1b52764e51ef709a9616991418fe00e
koefficiensdev <sandorturai178@gmail.com>
2026-09-04 12:03:12 +0200

    Guide outreach searches through verifiable listings

--- functions/test/outreach-research.test.js
+++ functions/test/outreach-research.test.js
@@ -2,7 +2,7 @@
 const test=require("node:test"),assert=require("node:assert/strict");
 const {discoveryTarget}=require("../outreach-research");
 test("research examines a larger bounded pool so rejected candidates can be replaced",()=>{
-  assert.equal(discoveryTarget(1),6);
+  assert.equal(discoveryTarget(1),10);
   assert.equal(discoveryTarget(5),15);
   assert.equal(discoveryTarget(20),20);
 });
